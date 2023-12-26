702ba842efecdff11c4da8b86f60fe8360612afd
purangi <kaya0728@naver.com>
2023-12-26 15:57:27 +0900

    card spread

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,8 +10,10 @@ public class GameManager : MonoBehaviour
 {
     public Text timeTxt;
     public float time;
+    Dictionary<GameObject, Vector3> cardList = new Dictionary<GameObject, Vector3>();
+    bool isCardGenerated;
 
-    public Text matchTxt;
+    public GameObject matchTxt;
     public GameObject card;
     public GameObject firstCard;
     public GameObject secondCard;
@@ -46,9 +48,42 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 1.0f;
         audioManager.SetActive(true);
-        int[] rtans = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 };
+
+        isCardGenerated = false;
+        generateCard();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isCardGenerated)
+        {
+            StartCoroutine(moveCard(0.03f));
+        }
+
+        if(isCardGenerated)
+        {
+            time += Time.deltaTime;
+            timeTxt.text = time.ToString("N2");
+
+            if (time > 20.0f)
+            {
+                timeTxt.text = "<color=red>" + time.ToString("N2") + "</color>"; // timeTxt의 색을 빨간색으로 변경
+            }
+
+            if (time > 30.0f)
+            {
+                GameEnd();
+            }
+        }
+    }
+
+    void generateCard()
+    {
+        int[] cards = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 };
         //OrderBy 정렬하겠다 Random.Range 랜덤한 순서로 ToArray() 배열로 만든다
-        rtans = rtans.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
+        cards = cards.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
 
         for (int i = 0; i < 20; i++)
         {
@@ -58,28 +93,29 @@ public class GameManager : MonoBehaviour
 
             float x = (i / 5) * 1.4f - 2.1f;
             float y = (i % 5) * 1.4f - 3.5f;
-            newCard.transform.position = new Vector3(x, y, 0);
+            newCard.transform.position = new Vector3(-2.1f, -3.5f, 0);
 
-            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = sprites[rtans[i]];
-            newCard.GetComponent<card>().cardName = sprites[rtans[i]].name.Substring(0,3);
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = sprites[cards[i]];
+            newCard.GetComponent<card>().cardName = sprites[cards[i]].name.Substring(0, 3);
+
+            cardList.Add(newCard, new Vector3(x, y, 0));
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator moveCard(float waitSeconds)
     {
-        time += Time.deltaTime;
-        timeTxt.text = time.ToString("N2");
-
-        if (time > 20.0f)
+        foreach (KeyValuePair<GameObject, Vector3> item in cardList)
         {
-            timeTxt.text = "<color=red>" + time.ToString("N2") + "</color>"; // timeTxt의 색을 빨간색으로 변경
+            GameObject card = item.Key;
+            Vector3 to = item.Value;
+
+            card.transform.position = Vector3.Lerp(card.transform.position, to, 0.5f);
+
+            //card.transform.rotation = Quaternion.Lerp(Quaternion.Euler(90f, 0, 0), Quaternion.Euler(0, 0, 0), 0.1f);
+            yield return new WaitForSeconds(waitSeconds);
         }
 
-        if (time > 30.0f)
-        {
-            GameEnd();
-        }
+        isCardGenerated = true;
     }
 
     public void isMatched()
@@ -95,7 +131,7 @@ public class GameManager : MonoBehaviour
         {
             //매칭 성공 -> 없애자
             audioSource.PlayOneShot(match);
-            matchTxt.text = firstCard.GetComponent<card>().cardName;
+            showTxt(firstCard.GetComponent<card>().cardName);
             firstCard.GetComponent<card>().destroyCard();
             secondCard.GetComponent<card>().destroyCard();
 
@@ -108,7 +144,7 @@ public class GameManager : MonoBehaviour
         } else
         {
             //덮자
-            matchTxt.text = "실패!";
+            showTxt("실패!");
             firstCard.GetComponent<card>().closeCard();
             secondCard.GetComponent<card>().closeCard();
 
@@ -121,6 +157,18 @@ public class GameManager : MonoBehaviour
 
         firstCard = null;
         secondCard = null;
+    }
+
+    void showTxt(string txt)
+    {
+        if (matchTxt.activeSelf == true)
+        {
+            matchTxt.SetActive(false);
+        }
+
+        matchTxt.GetComponent<Text>().text = txt;
+        //등장애니메이션 추가
+        matchTxt.SetActive(true);
     }
 
     void GameEnd()
